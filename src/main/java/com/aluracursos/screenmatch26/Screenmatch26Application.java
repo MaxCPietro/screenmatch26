85f55f68a3628ed896de15d8e77ba8f6a933634a
MaxCPietro <maxcorrea81@outlook.com>
2026-01-30 06:08:12 -0300

    hasta modelando las temporadas

--- src/main/java/com/aluracursos/screenmatch26/Screenmatch26Application.java
+++ src/main/java/com/aluracursos/screenmatch26/Screenmatch26Application.java
@@ -1,11 +1,16 @@
 package com.aluracursos.screenmatch26;
 
+import com.aluracursos.screenmatch26.model.DatosEpisodio;
 import com.aluracursos.screenmatch26.model.DatosSerie;
+import com.aluracursos.screenmatch26.model.DatosTemporada;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import service.ConsumoAPI;
 import service.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Screenmatch26Application implements CommandLineRunner {
@@ -25,7 +30,32 @@ public class Screenmatch26Application implements CommandLineRunner {
         ConvierteDatos conversor = new ConvierteDatos();
         var datos = conversor.obtenerDatos(json, DatosSerie.class);
         System.out.println("******DATOS SERIALIZADOS******");
-        System.out.println("datos SERIE:" + datos);
+        System.out.println("datos SERIE: " + datos);
+
+        //Datos Episodio
+        //cambio la url a url de episodio
+        json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=friends&season=1&episode=1&apikey=93082e3f");
+        DatosEpisodio episodios =  conversor.obtenerDatos(json, DatosEpisodio.class);
+        System.out.println("******DATOS SERIALIZADOS EPISODIO******");
+        System.out.println("datos EPISODIO: " + episodios);
+
+        //Datos de Temporadas
+        //cambio la url a url de episodio
+        // ✅ CORRECTO
+        List<DatosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= datos.TotalDeTemporadas(); i++) {
+            json = consumoAPI.obtenerDatos(
+                    "https://www.omdbapi.com/?t=friends&season=" + i + "&apikey=93082e3f"
+            );
+
+            DatosTemporada temporada = conversor.obtenerDatos(json, DatosTemporada.class);
+            temporadas.add(temporada);
+        }
+
+        System.out.println("******DATOS SERIALIZADOS TEMPORADA******");
+        System.out.println("datos TEMPORADA: " + temporadas);
+
 
     }
 }
