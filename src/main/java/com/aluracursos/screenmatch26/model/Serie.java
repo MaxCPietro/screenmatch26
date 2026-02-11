41fd0016d73b90f8cd16ec8b5fb7c91d00e43f3e
MaxCPietro <maxcorrea81@outlook.com>
2026-02-11 16:55:56 -0300

    hasta finalizacion de relacion serie con episodio

--- src/main/java/com/aluracursos/screenmatch26/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch26/model/Serie.java
@@ -27,7 +27,7 @@ public class Serie {
     private String actores;
     private String sinopsis;
 
-    @OneToMany (mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany (mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios;
 
     public Serie() {}
@@ -111,18 +111,20 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e->e.setSerie(this));
         this.episodios = episodios;
     }
 
     @Override
     public String toString() {
         return
-                "titulo='" + titulo + '\'' +
+                "titulo=" + titulo + '\'' +
                 ", totalDeTemporadas=" + totalDeTemporadas +
                 ", evaluacion=" + evaluacion +
                 ", poster='" + poster + '\'' +
                 ", genero=" + genero +
                 ", actores='" + actores + '\'' +
-                ", sinopsis='" + sinopsis + '\'';
+                ", sinopsis='" + sinopsis + '\'' +
+                ", episodios='" + episodios;
     }
 }
